0f7614beb2266aacd1a63dea86b56ec7e203a7a6
insane-03 <118243063+insane-03@users.noreply.github.com>
2022-11-28 18:40:29 +0530

    CRUD Operation using MongoDB

---
+++ Flopkart_Model/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flopkart_Model
+{
+    public class ProductModel
+    {
+        public string ? ProductName { get; set; }
+
+        public int  ProductId { get; set; }
+    }
+} 
